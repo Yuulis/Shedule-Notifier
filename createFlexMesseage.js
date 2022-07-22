2bbe3bc80dc2890b5c79e3ce5a0e7548dca01425
Yuulis <79734873+Yuulis@users.noreply.github.com>
2022-07-22 11:52:58 +0900

    flex messageの変更

--- createFlexMesseage.js
+++ createFlexMesseage.js
@@ -1,75 +1,101 @@
 function createFlexMessage(date, holiday, temp_h, temp_l, weather) {
   // flex messageの作成
   let flex = {
-    'type': 'bubble',
-    'size': 'giga',
-    'body': {
-      'type': 'box',
-      'layout': 'vertical',
-      'contents': [
+    "type": "bubble",
+    "size": "giga",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
         {
-          'type': 'text',
-          'text': date,
-          'weight': 'bold',
-          'size': 'xxl',
-          'flex': 0
-        }, {
-          'type': 'box',
-          'layout': 'horizontal',
-          'contents': [
-            {
-              'type': 'filler'
-            }, {
-              'type': 'text',
-              'text': weather,
-              'size': 'lg',
-              'color': '#444444',
-              'flex': 0,
-              'gravity': 'center'
-            }
-          ]
-        }, {
-          'type': 'box',
-          'layout': 'horizontal',
-          'contents': [
-            {
-              'type': 'filler'
-            }, {
-              'type': 'text',
-              'text': temp_l + '℃',
-              'size': 'lg',
-              'color': '#3f51b5',
-              'flex': 0
-            }, {
-              'type': 'text',
-              'text': '/',
-              'size': 'lg',
-              'color': '#444444',
-              'margin': 'xs',
-              'flex': 0
-            }, {
-              'type': 'text',
-              'text': temp_h + '℃',
-              'size': 'lg',
-              'color': '#f44336',
-              'margin': 'xs',
-              'flex': 0,
-              'gravity': 'center'
-            }
-          ]
-        }, {
-          'type': 'separator',
-          'margin': 'xl',
-          'color': '#808080'
-        }, {
-          'type': 'box',
-          'layout': 'vertical',
-          'contents': [
-            //EVENTS
-          ],
-          'margin': 'xl'
+          "type": "text",
+          "text": "Tomorrow's Events",
+          "size": "lg",
+          "style": "italic",
+          "color": "#444444",
+          "flex": 0
         }
       ]
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": date,
+          "size": "xxl",
+          "weight": "bold",
+          "color": "#444444",
+          "flex": 0,
+          "margin": "xs"
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "text": weather,
+              "align": "end",
+              "size": "lg",
+              "color": "#444444",
+              "gravity": "center"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "filler"
+            },
+            {
+              "type": "text",
+              "text": temp_l + '℃',
+              "flex": 0,
+              "color": "#3f51b5",
+              "size": "lg"
+            },
+            {
+              "type": "text",
+              "text": "/",
+              "flex": 0,
+              "color": "#444444",
+              "size": "lg",
+              "margin": "xs"
+            },
+            {
+              "type": "text",
+              "text": temp_h + '℃',
+              "margin": "xs",
+              "flex": 0,
+              "color": "#f44336",
+              "size": "lg",
+              "gravity": "center"
+            }
+          ]
+        },
+        {
+          "type": "separator",
+          "color": "#808080",
+          "margin": "xl"
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            // Events
+          ],
+          "margin": "xl"
+        }
+      ]
+    },
+    "styles": {
+      "header": {
+        "backgroundColor": "#a3ffa3"
+      }
     }
   };
 
